e3622b7dc08e4d00a07614e43e57da642856b05c
Bruno Finhana <finhana.bruno@gmail.com>
2025-04-16 15:53:52 -0300

    Creting the basic functionality of the counter on the script.js file

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 const counterNumberDisplay = document.querySelector("#counter-number-display")
 const subtractBtn = document.querySelector("#subtract-btn")
 const addBtn = document.querySelector("#add-btn")
+const resetBtn = document.querySelector("#reset-btn")
 
-var counterNumber = 10
+var counterNumber = 0
 
-counterNumberDisplay.textContent = counterNumber
+// Adding a number
+function add() {
+    counterNumber++
+    counterNumberDisplay.innerHTML = counterNumber
+}
+
+// Subtracting a number
+function subtract() {
+    // Preventing negative numbers
+    if (counterNumber <= 0) {
+        counterNumber = 0
+        // counterNumberDisplay.innerHTML = counterNumber
+        return
+    } else {
+        counterNumber--
+        counterNumberDisplay.innerHTML = counterNumber
+    }
+}
+
+// Resetting the counter
+function reset() {
+    counterNumber = 0
+    counterNumberDisplay.innerHTML = counterNumber
+}
+
+// Adding event listeners to buttons
+addBtn.addEventListener("click", add)
+subtractBtn.addEventListener("click", subtract)
+resetBtn.addEventListener("click", reset)
