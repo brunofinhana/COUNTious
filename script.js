0ca6f4039d4e029f163fb7eed43c01ac6b10ca4d
Bruno Finhana <42972701+brunofinhana@users.noreply.github.com>
2025-04-19 18:52:18 -0300

    Refactoring the reset function to display a confirmation modal and clear the counter title

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const counterNumberDisplay = document.querySelector("#counter-number-display")
+const counterTitle = document.querySelector("#counter-title")
 const subtractBtn = document.querySelector("#subtract-btn")
 const addBtn = document.querySelector("#add-btn")
 const resetBtn = document.querySelector("#reset-btn")
+const resetConfirmation = document.querySelector("#reset-confirmation")
 
 var counterNumber = 0
 
@@ -26,8 +28,26 @@ function subtract() {
 
 // Resetting the counter
 function reset() {
-    counterNumber = 0
-    counterNumberDisplay.innerHTML = counterNumber
+    		
+			// Show confirmation modal
+			resetConfirmation.display = block
+
+			const resetYes = document.querySelector("#reset-yes")
+			const resetNo  = document.querySelector("#reset-no")
+
+			// ISSUE #1: Refactor the reset button
+			resetYes.addEventListening("click", () => {
+						counterNumber = 0
+    			counterNumberDisplay.innerHTML = counterNumber
+
+						// Cleaning the title
+						counterTitle.textContent = ""
+			})
+
+			resetNo.addEventListening("click", {
+						resetConfirmation.display = none
+			})
+
 }
 
 // Adding event listeners to buttons
